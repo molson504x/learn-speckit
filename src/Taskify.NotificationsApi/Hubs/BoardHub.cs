4302224e87995c205c1ba341eca2ec2b4fa07360
@molson504x <4873425+molson504x@users.noreply.github.com>
2026-08-23 22:49:14 -0400

    Revert "Apply suggestions from code review"
    This reverts commit f568529b956fe1050f6724c5b3ed6e8aa1929492.

--- src/Taskify.NotificationsApi/Hubs/BoardHub.cs
+++ src/Taskify.NotificationsApi/Hubs/BoardHub.cs
@@ -5,22 +5,8 @@ namespace Taskify.NotificationsApi.Hubs;
 public sealed class BoardHub : Hub
 {
     public Task JoinProject(string projectId)
-    {
-        if (!Guid.TryParse(projectId, out var parsedProjectId))
-        {
-            throw new HubException("Invalid projectId.");
-        }
-
-        return this.Groups.AddToGroupAsync(this.Context.ConnectionId, parsedProjectId.ToString("D"));
-    }
+        => this.Groups.AddToGroupAsync(this.Context.ConnectionId, projectId);
 
     public Task LeaveProject(string projectId)
-    {
-        if (!Guid.TryParse(projectId, out var parsedProjectId))
-        {
-            throw new HubException("Invalid projectId.");
-        }
-
-        return this.Groups.RemoveFromGroupAsync(this.Context.ConnectionId, parsedProjectId.ToString("D"));
-    }
+        => this.Groups.RemoveFromGroupAsync(this.Context.ConnectionId, projectId);
 }
