14c3fc41bfbd8c02ded1bb6734980d1bb61310b5
@molson504x <4873425+molson504x@users.noreply.github.com>
2026-08-23 22:49:13 -0400

    Revert "Refactor API client base addresses to use configuration settings"
    This reverts commit 5a1656334f942825149c557d30016bc4d71b0fa5.

--- src/Taskify.Web/Program.cs
+++ src/Taskify.Web/Program.cs
@@ -11,20 +11,19 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddProtectedBrowserStorage();
-
 builder.Services.AddHttpClient<ProjectsApiClient>(client =>
 {
-    client.BaseAddress = new Uri(builder.Configuration["Services:ProjectsApi:BaseUrl"] ?? "https://localhost:5001");
+    client.BaseAddress = new Uri("https+http://projectsapi");
 });
 
 builder.Services.AddHttpClient<TasksApiClient>(client =>
 {
-    client.BaseAddress = new Uri(builder.Configuration["Services:TasksApi:BaseUrl"] ?? "https://localhost:5002");
+    client.BaseAddress = new Uri("https+http://tasksapi");
 });
 
 builder.Services.AddHttpClient<NotificationsApiClient>(client =>
 {
-    client.BaseAddress = new Uri(builder.Configuration["Services:NotificationsApi:BaseUrl"] ?? "https://localhost:5003");
+    client.BaseAddress = new Uri("https+http://notificationsapi");
 });
 
 var app = builder.Build();
